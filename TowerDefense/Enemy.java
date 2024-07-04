9db1cf5b625f7faea6abb734ffd787a74408599c
LAPTOP-LEHNIO12\Ucitel <milan.apolenik@oapb.sk>
2024-07-04 14:59:13 +0200

    First tower

---
+++ TowerDefense/Enemy.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The best game.
+ * 
+ * @author (Milan) 
+ * @version (1.0 04.07.2024)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * This is enemy
+     * of the state
+     */
+    public void act()
+    {
+        // Add your action code here.
+        this.move(2);
+    }
+}
